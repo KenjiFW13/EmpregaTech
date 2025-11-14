49c8bf0c1f89e398459cfb15e7667db961b3fbaa
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2025-11-14 14:35:44 -0300

    Criação do Construtor Vaga

--- src/entities/Vaga.java
+++ src/entities/Vaga.java
@@ -5,4 +5,15 @@ public class Vaga {
     String titulo, area, tipoContrato, nivel;
     boolean vagaInclusiva;
     Empresa empresa;
+
+    public Vaga(int id, String titulo, String area, String tipoContrato, String nivel, boolean vagaInclusiva, Empresa empresa) {
+        this.id = id;
+        this.titulo = titulo;
+        this.area = area;
+        this.tipoContrato = tipoContrato;
+        this.nivel = nivel;
+        this.vagaInclusiva = vagaInclusiva;
+        this.empresa = empresa;
+
+    }
 }
