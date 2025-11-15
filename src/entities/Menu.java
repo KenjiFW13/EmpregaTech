bf89afb57e0151b6a8364e3889ff1cd4375ad300
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2025-11-14 22:30:45 -0300

    Criação do método cadastarEmpresa em Menu

--- src/entities/Menu.java
+++ src/entities/Menu.java
@@ -1,7 +1,8 @@
 package entities;
+import java.util.Scanner;
 
 public class Menu {
-
+    Scanner scanner = new Scanner(System.in);
     private Repositorio repositorio;
 
     public Menu(Repositorio repositorio) {
@@ -9,9 +10,44 @@ public class Menu {
     }
 
     public void exibirMenu(){
+
         while (true) {
             System.out.println("""
-                    === MENU EmpregaTech ===""");
+                    === MENU EmpregaTech ===
+                    1 - Cadastrar Empresa
+                    2 - Cadastrar Vaga
+                    3 - Listar Empresas
+                    4 - Listar Vagas
+                    0 - Sair
+                    =========================""");
+            int opcao = scanner.nextInt();
+            switch (opcao) {
+                case 0: return;
+                default: System.out.println("Insira uma opção válida!");
+            }
         }
+    }
+
+    public void cadastrarEmpresa(Empresa empresa) {
+        System.out.println("=== Cadastrar Empresa ===");
+
+        System.out.println("Nome do Empresa: ");
+        String nome = scanner.next();
+
+        System.out.println("E-mail da Empresa: ");
+        String email = scanner.next();
+
+        System.out.println("Setor da Empresa: ");
+        String setor = scanner.next();
+
+        System.out.println("A empresa possui políticas ativas de inclusão e diversidade?\n" +
+                "1 - Sim\n" +
+                "2 - Não\n");
+        int opcao = scanner.nextInt();
+        boolean compromissoInclusao = (opcao == 1);
+
+        repositorio.adicionarEmpresa(empresa);
+
+        System.out.println("Empresa adicionada com sucesso!");
     }
 }
