42f9fec0940aa12bfd56387dc55d0345da9be508
kenjifw13 <kenji.fernandes.wakabayashi@gmail.com>
2025-11-16 22:08:02 -0300

    Main desenvolvida

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import entities.Menu;
+import entities.Repositorio;
+
 void main() {
+    Repositorio repositorio = new Repositorio();
+    // Repositorio será usado como uma "memória do sistema"
 
+    Menu menu = new Menu(repositorio);
+
+    menu.exibirMenu();
     }
-
